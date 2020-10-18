015d8fffc45ad34989a09232b1fe4228289007a5
Omer Cavdar <omer.cavdar@getir.com>
2020-10-19 01:29:35 +0300

    Add html files with styles and template engine

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,17 +1,37 @@
+const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+app.use(express.static(publicDirectoryPath))
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath)
 
 app.get('', (req, res) => {
-    res.send('Hello express!');
-})
-
-app.get('/help', (req, res) => {
-    res.send('Help page!');
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Omer Cavdar',
+    });
 })
 
 app.get('/about', (req, res) => {
-    res.send('<h1>About</h1>');
+    res.render('about', {
+        title: 'About Me',
+        name: 'Omer Cavdar',
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        name: 'Omer Cavdar',
+        message: 'This message is an example message',
+    })
 })
 
 app.get('/weather', (req, res) => {
@@ -19,6 +39,22 @@ app.get('/weather', (req, res) => {
         forecast: 'It is snowing',
         location: 'Istanbul'
     });
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Omer Cavdar',
+        errorMessage: 'Help article not found.'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Omer Cavdar',
+        errorMessage: 'Page not found.'
+    })
 })
 
 app.listen(8000, () => {
